449fb31e19e5008e2c7c8b5e1779faa0d1e31a32
Luna <128090844+peqlua@users.noreply.github.com>
2024-08-19 13:54:27 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+  String sentimento = "amor❤️";
+  String grau = "muito frio";
+  int idade = 42;
+  int porcentoamor = 100;
+  string programaTVfavorito = "naturescape";   
+    Console.WriteLine ("Hello World");
+  }
+}
